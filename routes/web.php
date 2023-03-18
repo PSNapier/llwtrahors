0f6081b4be1ada2e35b4188ba399ae3b3062ad69
PSNapier <46052087+PSNapier@users.noreply.github.com>
2023-03-18 16:11:57 -0500

    feat: nav bar, started genetics page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/genetics', function () {
+    return view('genetics');
+});
+
 Route::get('/', function () {
     return view('landing');
 });
